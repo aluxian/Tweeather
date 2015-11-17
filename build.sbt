4f8cd3c7ec184831b7aeddd30aaba7b6785b0ca4
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-11-17 14:04:30 +0200

    Creates the Sentiment140Parser class

--- build.sbt
+++ build.sbt
@@ -8,5 +8,7 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "1.5.2",
   "org.apache.spark" %% "spark-streaming" % "1.5.2",
   "org.apache.spark" %% "spark-streaming-twitter" % "1.5.2",
-  "org.apache.spark" %% "spark-mllib" % "1.5.2"
+  "org.apache.spark" %% "spark-mllib" % "1.5.2",
+  "org.apache.hadoop" % "hadoop-client" % "2.7.1",
+  "com.github.fommil.netlib" % "all" % "1.1.2" pomOnly()
 )
---
+++ src/main/scala/com/aluxian/tweeather/Sentiment140Parser.scala
@@ -0,0 +1,48 @@
+package com.aluxian.tweeather
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Sentiment140Parser {
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf()
+      .setAppName("Tweeather_" + Sentiment140Parser.getClass.getSimpleName)
+      .setMaster(sys.env.getOrElse("TW_MASTER", "local[2]"))
+
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+
+    val testData = sc.textFile("hdfs://localhost:9000/tw/sentiment140/test.csv", 2)
+    val trainingData = sc.textFile("hdfs://localhost:9000/tw/sentiment140/training.csv", 8)
+
+    parse(sqlContext, testData, "hdfs://localhost:9000/tw/parsed/test.parquet")
+    parse(sqlContext, trainingData, "hdfs://localhost:9000/tw/parsed/training.parquet")
+  }
+
+  def parse(sqlContext: SQLContext, data: RDD[String], filePath: String) {
+    var parsed = data
+      .filter(_.contains("\",\"")) // ensure correct format
+      .map(_.split("\",\"").map(_.replace("\"", ""))) // split columns and remove " marks
+      .filter(row => row.forall(_.nonEmpty)) // ensure columns are not empty
+      .map(row => (row(0).toInt, row(5))) // keep sentiment and text only
+      .filter(row => row._1 != 2) // remove neutral tweets
+      .map(row => (row._1 / 4, row._2)) // normalize sentiment
+
+    // Sanitize text
+    parsed = parsed.map(row => (row._1, row._2
+      .replaceAll("&amp;|&gt;|&lt;|&quot;|&#39;", "") // html entities
+      .replaceAll("[\\uE000-\\uF8FF]", "") // unicode symbols
+      .replaceAll("https?:\\/\\/[^\\s]*", "") // urls
+      .replaceAll("\\bvia\\b|\\bRT\\b", "") // 'via' and 'RT' keywords
+      .replaceAll("\\s+", " ") // multiple white spaces
+      .replaceAll("\\B@\\w*", "") // @ mentions
+      .trim)
+    )
+
+    import sqlContext.implicits._
+    parsed.toDF("sentiment", "text").write.save(filePath)
+  }
+
+}
