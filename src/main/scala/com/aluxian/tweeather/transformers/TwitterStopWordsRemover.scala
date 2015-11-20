ac5efe446745165c045ae76d48e3ba7ba2f2cd3f
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-11-20 12:33:21 +0200

    Moves words for TwitterStopWordsRemover into a separate Seq

--- src/main/scala/com/aluxian/tweeather/transformers/TwitterStopWordsRemover.scala
+++ src/main/scala/com/aluxian/tweeather/transformers/TwitterStopWordsRemover.scala
@@ -5,9 +5,13 @@ import org.apache.spark.ml.util.Identifiable
 
 class TwitterStopWordsRemover(override val uid: String) extends StopWordsRemover(uid) {
 
+  val twitterStopWords = Seq(
+    "RT"
+  )
+
   def this() = {
     this(Identifiable.randomUID("twitterStopWords"))
-    setStopWords($(stopWords) :+ "RT")
+    setStopWords($(stopWords) ++ twitterStopWords)
   }
 
 }
