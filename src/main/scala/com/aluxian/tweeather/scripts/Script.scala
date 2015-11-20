b70c8cbb54df821c120fd88841990336a5aeb90b
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-11-20 10:27:58 +0200

    Adds hdfs var to Script, removes $ suffix from app name

--- src/main/scala/com/aluxian/tweeather/scripts/Script.scala
+++ src/main/scala/com/aluxian/tweeather/scripts/Script.scala
@@ -1,11 +1,16 @@
-package com.aluxian.tweeather
+package com.aluxian.tweeather.scripts
 
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
 import org.apache.spark.{SparkConf, SparkContext}
 
 trait Script {
 
-  implicit val scriptName: String = this.getClass.getSimpleName
+  implicit var scriptName: String = this.getClass.getSimpleName.stripSuffix("$")
   implicit var hdfsHostUrl: String = "hdfs://localhost:9000"
+
+  lazy val hdfsConf = new Configuration()
+  lazy val hdfs = FileSystem.get(hdfsConf)
 
   implicit class HdfsStringInterpolator(val sc: StringContext) {
     def hdfs(args: Any*): String = hdfsHostUrl + sc.parts.mkString
@@ -13,6 +18,7 @@ trait Script {
 
   def main(implicit args: Array[String]) {
     hdfsHostUrl = arg("--hdfs").getOrElse(hdfsHostUrl)
+    hdfsConf.set("fs.default.name", hdfsHostUrl)
 
     val conf = new SparkConf()
       .setAppName("Tweeather_" + scriptName)
