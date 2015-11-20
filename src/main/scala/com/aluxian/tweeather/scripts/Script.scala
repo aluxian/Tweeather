950cb10e1a32dc4757e04a4dea2bce446d4c6f8d
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-11-20 13:47:45 +0200

    TwitterStream test; twitter4j auth credentials through env vars

--- src/main/scala/com/aluxian/tweeather/scripts/Script.scala
+++ src/main/scala/com/aluxian/tweeather/scripts/Script.scala
@@ -7,8 +7,6 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 trait Script {
 
-  var args: Array[String] = _
-
   implicit var scriptName: String = this.getClass.getSimpleName.stripSuffix("$")
   implicit var hdfsHostUrl: String = "hdfs://localhost:9000"
 
@@ -19,33 +17,26 @@ trait Script {
     def hdfs(args: Any*): String = hdfsHostUrl + sc.s(args: _*)
   }
 
-  def main(args: Array[String]) {
-    this.args = args
-
+  def main(implicit args: Array[String]) {
     Option(getClass.getClassLoader.getResource("log4j.properties")) match {
       case Some(url) => PropertyConfigurator.configure(url)
       case None => System.err.println("Unable to load log4j.properties")
     }
 
-    hdfsHostUrl = arg("--hdfs").getOrElse(hdfsHostUrl)
+    hdfsHostUrl = arg("hdfs").getOrElse(hdfsHostUrl)
     hdfsConf.set("fs.defaultFS", hdfsHostUrl)
 
     val conf = new SparkConf()
       .setAppName("Tweeather_" + scriptName)
-      .setMaster(arg("--master").getOrElse("local[2]"))
+      .setMaster(arg("master").getOrElse("local[2]"))
       .set("spark.app.id", "Tweeather")
 
     main(new SparkContext(conf))
   }
 
-  def arg(name: String): Option[String] = {
-    val pi = args.indexOf(name)
-
-    if (args.length > pi + 1) {
-      return Option(args(pi + 1))
-    }
-
-    Option.empty
+  def arg(name: String)(implicit args: Array[String]): Option[String] = {
+    val argName = "--" + name + "="
+    args.find(_.startsWith(argName)).map(_.stripPrefix(argName))
   }
 
   def main(sc: SparkContext)
--- src/main/scala/com/aluxian/tweeather/scripts/TwitterStream.scala
+++ src/main/scala/com/aluxian/tweeather/scripts/TwitterStream.scala
@@ -3,22 +3,21 @@ package com.aluxian.tweeather.scripts
 import org.apache.spark.streaming.twitter.TwitterUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{Logging, SparkContext}
-import twitter4j.TwitterFactory
-import twitter4j.auth.{AccessToken, Authorization}
 
 object TwitterStream extends Script with Logging {
 
   def main(sc: SparkContext) {
     val ssc = new StreamingContext(sc, Seconds(10))
-    val stream = TwitterUtils.createStream(ssc, twitterAuth())
-//    stream.getReceiver().
-  }
+    val stream = TwitterUtils.createStream(ssc, None)
 
-  def twitterAuth(): Option[Authorization] = {
-    val twitter = new TwitterFactory().getInstance()
-    twitter.setOAuthConsumer(arg("--twitterConsumerKey").get, arg("--twitterConsumerSecret").get)
-    twitter.setOAuthAccessToken(new AccessToken(arg("--twitterToken").get, arg("--twitterTokenSecret").get))
-    Option(twitter.getAuthorization)
+    stream.foreachRDD(rdd => {
+      rdd.foreach(status => {
+        println(s"${status.getId} ${status.getUser.getScreenName} ${status.getText}")
+      })
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
   }
 
 }
