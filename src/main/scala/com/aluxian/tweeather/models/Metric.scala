441813808d1690039dc48fc8548a4554f1724f27
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-12-07 13:45:39 +0200

    Fixes static Metric types

--- src/main/scala/com/aluxian/tweeather/models/Metric.scala
+++ src/main/scala/com/aluxian/tweeather/models/Metric.scala
@@ -3,7 +3,7 @@ package com.aluxian.tweeather.models
 case class Metric(gridName: String, name: String)
 
 object Metric {
-  val Pressure = Metric("Pressure_surface", "temperature")
-  val Temperature = Metric("Temperature_surface", "pressure")
+  val Pressure = Metric("Pressure_surface", "pressure")
+  val Temperature = Metric("Temperature_surface", "temperature")
   val Humidity = Metric("Relative_humidity_height_above_ground", "humidity")
 }
