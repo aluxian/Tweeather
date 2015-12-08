f79e9ad5e1442ae0453d1c5b0609f5e33cd71504
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-12-08 16:36:43 +0200

    Moves implicit formats field inside method in Param.scala

--- src/main/scala/com/aluxian/tweeather/utils/Param.scala
+++ src/main/scala/com/aluxian/tweeather/utils/Param.scala
@@ -17,16 +17,16 @@ class LocationBoxParam(parent: Params, name: String, doc: String, isValid: Locat
   def this(parent: Params, name: String, doc: String) =
     this(parent, name, doc, Validators.alwaysTrue)
 
-  implicit val formats = DefaultFormats
-
   /** Creates a param pair with the given value (for Java). */
   override def w(value: LocationBox): ParamPair[LocationBox] = super.w(value)
 
   override def jsonEncode(value: LocationBox): String = {
+    implicit val formats = DefaultFormats
     Serialization.write[LocationBox](value)
   }
 
   override def jsonDecode(json: String): LocationBox = {
+    implicit val formats = DefaultFormats
     parse(json).extract[LocationBox]
   }
 
@@ -41,16 +41,16 @@ class MetricArrayParam(parent: Params, name: String, doc: String, isValid: Array
   def this(parent: Params, name: String, doc: String) =
     this(parent, name, doc, Validators.alwaysTrue)
 
-  implicit val formats = DefaultFormats
-
   /** Creates a param pair with a [[java.util.List]] of values (for Java and Python). */
   def w(value: java.util.List[Metric]): ParamPair[Array[Metric]] = w(value.asScala.toArray)
 
   override def jsonEncode(value: Array[Metric]): String = {
+    implicit val formats = DefaultFormats
     Serialization.write[Array[Metric]](value)
   }
 
   override def jsonDecode(json: String): Array[Metric] = {
+    implicit val formats = DefaultFormats
     parse(json).extract[Array[Metric]]
   }
 
