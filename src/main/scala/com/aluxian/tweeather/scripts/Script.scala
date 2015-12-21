f5f3b2b4decce51d8d0af2e61b7d88662bc835ca
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-12-21 23:29:01 +0200

    Fixes removed imports

--- src/main/scala/com/aluxian/tweeather/scripts/Script.scala
+++ src/main/scala/com/aluxian/tweeather/scripts/Script.scala
@@ -2,7 +2,8 @@ package com.aluxian.tweeather.scripts
 
 import org.apache.hadoop.fs.FileSystem
 import org.apache.log4j.PropertyConfigurator
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.streaming.{Minutes, Seconds}
+import org.apache.spark.{SparkConf, SparkContext}
 
 trait Script {
 
