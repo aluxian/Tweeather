006c0bc26149ebfbe9e9c8291c4338786b5ddd2c
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-01-08 12:19:37 +0200

    Refactors spark submit tasks file

--- project/SparkSubmit.scala
+++ project/SparkSubmit.scala
@@ -1,60 +1,45 @@
 import sbtsparksubmit.SparkSubmitPlugin.autoImport._
 
-sealed case class Config(name: String, memory: Option[Int] = None)
+import scala.collection.mutable
 
 object SparkSubmit {
-  lazy val configurations = SparkSubmitSetting(
-    SparkSubmitSetting("submit-ClusterTest", Seq(
-      "--class", "com.aluxian.tweeather.scripts.ClusterTest",
-      "--master", "spark://spark.r.tweeather.aluxian.com:7077"
-    )),
-    SparkSubmitSetting("submit-Sentiment140Downloader", Seq(
-      "--class", "com.aluxian.tweeather.scripts.Sentiment140Downloader"
-    )),
-    SparkSubmitSetting("submit-Sentiment140Parser", Seq(
-      "--class", "com.aluxian.tweeather.scripts.Sentiment140Parser"
-    )),
-    SparkSubmitSetting("submit-Sentiment140Trainer", Seq(
-      "--class", "com.aluxian.tweeather.scripts.Sentiment140Trainer"
-    )),
-    SparkSubmitSetting("submit-Sentiment140Repl", Seq(
-      "--class", "com.aluxian.tweeather.scripts.Sentiment140Repl"
-    )),
-    SparkSubmitSetting("submit-TwitterEmoCollector", Seq(
-      "--class", "com.aluxian.tweeather.scripts.TwitterEmoCollector"
-    )),
-    SparkSubmitSetting("submit-TwitterEmoCounter", Seq(
-      "--class", "com.aluxian.tweeather.scripts.TwitterEmoCounter",
-      "--executor-memory", "14.4g"
-    )),
-    SparkSubmitSetting("submit-TwitterEmoParser", Seq(
-      "--class", "com.aluxian.tweeather.scripts.TwitterEmoParser",
-      "--executor-memory", "14.4g"
-    )),
-    SparkSubmitSetting("submit-TwitterEmoTrainer", Seq(
-      "--class", "com.aluxian.tweeather.scripts.TwitterEmoTrainer",
-      "--executor-memory", "14.4g"
-    )),
-    SparkSubmitSetting("submit-TwitterEmoRepl", Seq(
-      "--class", "com.aluxian.tweeather.scripts.TwitterEmoRepl"
-    )),
-    SparkSubmitSetting("submit-TwitterFireCollector", Seq(
-      "--class", "com.aluxian.tweeather.scripts.TwitterFireCollector"
-    )),
-    SparkSubmitSetting("submit-TwitterFireCounter", Seq(
-      "--class", "com.aluxian.tweeather.scripts.TwitterFireCounter",
-      "--executor-memory", "14.4g"
-    )),
-    SparkSubmitSetting("submit-TwitterFireParser", Seq(
-      "--class", "com.aluxian.tweeather.scripts.TwitterFireParser",
-      "--executor-memory", "14.4g"
-    )),
-    SparkSubmitSetting("submit-TwitterFireTrainer", Seq(
-      "--class", "com.aluxian.tweeather.scripts.TwitterFireTrainer",
-      "--executor-memory", "14.4g"
-    )),
-    SparkSubmitSetting("submit-TwitterFireRepl", Seq(
-      "--class", "com.aluxian.tweeather.scripts.TwitterFireRepl"
-    ))
+
+  private lazy val sparkMaster = sys.props.getOrElse("SPARK_MASTER", "local[*]")
+  private lazy val executorHighMem = "14.4g"
+
+  private sealed case class Script(scriptName: String, highMem: Boolean = false) {
+    def toSparkSubmit = {
+      val params = mutable.MutableList(
+        "--class", s"com.aluxian.tweeather.scripts.$scriptName",
+        "--master", sparkMaster
+      )
+
+      if (highMem) {
+        params ++= "--executor-memory" :: executorHighMem :: Nil
+      }
+
+      SparkSubmitSetting(s"submit-$scriptName", params)
+    }
+  }
+
+  private lazy val configs = Seq(
+    Script("ClusterTest"),
+    Script("Sentiment140Downloader"),
+    Script("Sentiment140Parser"),
+    Script("Sentiment140Trainer"),
+    Script("Sentiment140Repl"),
+    Script("TwitterEmoCollector"),
+    Script("TwitterEmoCounter", highMem = true),
+    Script("TwitterEmoParser", highMem = true),
+    Script("TwitterEmoTrainer", highMem = true),
+    Script("TwitterEmoRepl"),
+    Script("TwitterFireCollector", highMem = true),
+    Script("TwitterFireCounter", highMem = true),
+    Script("TwitterFireParser", highMem = true),
+    Script("TwitterFireTrainer"),
+    Script("TwitterFireRepl")
   )
+
+  lazy val configurations = SparkSubmitSetting(configs.map(_.toSparkSubmit): _*)
+
 }
